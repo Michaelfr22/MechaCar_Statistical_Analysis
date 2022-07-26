906ad006f53db23215ced3052f0070bfd662515f
Michaelfr22 <102773052+Michaelfr22@users.noreply.github.com>
2022-07-25 23:06:23 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,6 @@
 # MechaCar Statistical Analysis
 
-## Deliverable
+## Deliverable 1
 
 mecha_car <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #import used car dataset
 
@@ -25,3 +25,14 @@ summary(lm(mpg ~
 #### Findings
 ##### 1. R-squared: 0.7149
 ##### 2. p-value:   5.35 x 10^-11
+
+
+## Deliverable 2
+
+susp_coil <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #import used car dataset
+
+total_summary <- data.frame(Suspension_Coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI), .groups = 'keep'))
+total_summary
+
+lot_summary <- data.frame(Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI), .groups = 'keep'))
+lot_summary
